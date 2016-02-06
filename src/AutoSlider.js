18a7d4369978fe74caf94fec678960dea3632eba
rascada <carftjr@gmail.com>
2016-02-06 22:06:28 +0100

    update to new apis

--- src/AutoSlider.js
+++ src/AutoSlider.js
@@ -3,23 +3,23 @@
 import log from '@sled/log';
 
 class AutoSlider {
-  constructor($core) {
+  init(core) {
     this.work = false;
     this.interval = 5000;
 
-    this.slides = $core.modules.slides;
+    this.slides = core.modules.slides;
     this.slides.on('change', n => this.restart());
 
-    if (!$core.domModules.autoSlider) {
-      log(`[${$core.id}]`, '[ modules-dom ]', 'missing autoSlider');
+    if (!this.$) {
+      log({ id: core.id }, '[ modules-dom ]', 'missing autoSlider');
     } else {
-      this.autoSlider = $core.domModules.autoSlider.firstElementChild;
-      log(`[${$core.id}]`, '[ modules-dom ]', 'succesfully integrate with dom-autoSlider');
+      this.autoSlider = this.$.firstElementChild;
+      log({ id: core.id }, '[ modules-dom ]', 'succesfully integrate with dom-autoSlider');
     }
   }
 
   start(interval) {
-    if (this.slides.$slides.children.length > 1) {
+    if (this.slides.$.children.length > 1) {
       this.restarting = false;
       log('autoSlider ruszył');
 
@@ -32,19 +32,23 @@ class AutoSlider {
       this.work = true;
       this.heart = setInterval(_=> {
         !this.slides.change(1) ? this.stop() : null;
-        if (this.autoSlider) {
-          new Promise(end => {
-            this.autoSlider.style.webkitTransitionDuration = '0ms';
-            this.autoSlider.style.transitionDuration = '0ms';
-            this.autoSlider.style.width = '0';
-            setTimeout(end, 25);
-          }).then(_=> {
-            this.autoSlider.style.transitionDuration = (interval || this.interval) + 'ms';
-            this.autoSlider.style.webkitTransitionDuration = (interval || this.interval) + 'ms';
-            this.autoSlider.style.width = '100%';
-          });
-        }
+        this.progressBar();
       }, (interval || this.interval) + 25);
+    }
+  }
+
+  progressBar() {
+    if (this.autoSlider) {
+      new Promise(end => {
+        this.autoSlider.style.webkitTransitionDuration = '0ms';
+        this.autoSlider.style.transitionDuration = '0ms';
+        this.autoSlider.style.width = '0';
+        setTimeout(end, 25);
+      }).then(_=> {
+        this.autoSlider.style.transitionDuration = (interval || this.interval) + 'ms';
+        this.autoSlider.style.webkitTransitionDuration = (interval || this.interval) + 'ms';
+        this.autoSlider.style.width = '100%';
+      });
     }
   }
 
@@ -59,8 +63,10 @@ class AutoSlider {
 
   stop() {
     log('autoSlider został zatrzymany');
+
     if (this.autoSlider)
         this.autoSlider.style.width = 0;
+
     this.work = false;
     clearInterval(this.heart);
   }
